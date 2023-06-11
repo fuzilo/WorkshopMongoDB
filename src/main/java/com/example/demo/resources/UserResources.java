cb97a87928470857acd51d4bb44bc9e978ec8197
Ricardo Matsumura <ricardo.matsumura.pb@compasso.com.br>
2023-06-11 11:29:59 -0300

    Entity e Resst funcionando

--- src/main/java/com/example/demo/resources/UserResources.java
+++ src/main/java/com/example/demo/resources/UserResources.java
@@ -1,5 +1,27 @@
 package com.example.demo.resources;
 
-public class UserResources {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.User;
+
+@RestController
+@RequestMapping (value="/users")
+public class UserResources {
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		User maria =  new User("1", "Maria Silva", "maria123@gmail.com");
+		User alex =  new User("2", "Alex Green", "agree123@gmail.com");
+		
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria,alex));
+		return ResponseEntity.ok().body(list);
+		
+	}
 }
